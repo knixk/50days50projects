94ce6a0be913b1bed9979397745ff15378098c7f
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2023-09-30 15:53:24 +0530

    done

--- expanding-cards/app.js
+++ expanding-cards/app.js
@@ -1,1 +1,17 @@
 const panels = document.querySelectorAll('.panel')
+
+// You can't map over these
+// Map returns a new arr
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
