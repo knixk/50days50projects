656f3abab9e70f0ccb85cbf7888c2fb6ff57a93f
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2023-10-04 20:02:38 +0530

    Update app.js

---
+++ scroll-animation/app.js
@@ -0,0 +1,27 @@
+const boxes = document.querySelectorAll('.box')
+
+function checkBoxes() {
+
+    const triggerBottom = (window.innerHeight / 5) * (4);
+
+    boxes.forEach((box) => {
+
+        const boxTop = box.getBoundingClientRect().top;
+
+        if (boxTop < triggerBottom) {
+            // when the rectangle is above the breakpoint
+            box.classList.add('show');
+            
+        } else {
+
+            box.classList.remove('show');
+            
+        }
+
+    })
+
+}
+
+checkBoxes();
+
+window.addEventListener("scroll", checkBoxes);
