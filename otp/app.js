83992a1a1c95d98bb66cadbfda1f8ce8dca7cb4e
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-08-21 16:25:58 +0530

    changes

--- otp/app.js
+++ otp/app.js
@@ -4,24 +4,24 @@ codes[0].focus();
 
 codes.forEach((code, idx) => {
   code.addEventListener("keydown", (e) => {
-    if (idx >= 0 && idx <= 5) {
-      setTimeout(() => {
-        if (e.key >= 0 && e.key <= 9) {
-          if (idx == 5) {
-            const val = codes[idx].value;
-            return;
-          }
-          codes[++idx].focus();
+    setTimeout(() => {
+      const key = codes[idx].value;
+      if (e.key >= 0 && e.key <= 9) {
+        if (idx == 5) {
+          const val = codes[idx].value;
+          return;
         }
-        if (e.key === "Backspace" || e.key === "Delete") {
-          if (idx == 0) {
-            return;
-          }
-          codes[idx].value = "";
-          codes[--idx].focus();
+
+        codes[++idx].focus();
+      }
+      if (e.key === "Backspace" || e.key === "Delete") {
+        if (idx == 0) {
+          return;
         }
+        // codes[idx].value = "";
+        codes[--idx].focus();
         console.log(idx);
-      }, 10);
-    }
+      }
+    }, 50);
   });
 });
