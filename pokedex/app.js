fe19524e94aa6347aa5640be565066d95ad2115f
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-09-14 00:02:39 +0530

    changes

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,5 +1,19 @@
 const menuBtn = document.querySelector(".menu");
 
+const sidebar = document.querySelector(".sidebar");
+
+const pokeContainer = document.querySelector(".poke-container");
+
 menuBtn.addEventListener("click", () => {
-  console.log("open nav");
+  sidebar.classList.toggle("hide");
+
+  console.log(menuBtn.textContent);
+
+  if (menuBtn.textContent == "O") {
+    menuBtn.textContent = "X";
+    pokeContainer.classList.add("opacity");
+  } else {
+    menuBtn.textContent = "O";
+    pokeContainer.classList.remove("opacity");
+  }
 });
