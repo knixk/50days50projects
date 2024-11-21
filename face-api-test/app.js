6410213f4919977d6b4931ae16449876926900c6
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-11-21 17:28:00 +0530

    changes

--- face-api-test/app.js
+++ face-api-test/app.js
@@ -1,3 +1,19 @@
+const video = document.getElementById("video");
 
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+]).then(startVideo);
 
+// 640 x 480 is the camera o / p
 
+function startVideo() {
+  // console.log(navigator.getUserMedia);
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
+}
