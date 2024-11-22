5157d453ac7370b1cd9264232abb7fa5aa4ad5bb
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-11-22 18:54:49 +0530

    react

--- waiver-react/src/App.jsx
+++ waiver-react/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app__container">
+      <nav class="nav">WaiverForm</nav>
+
+      <main class="form__wrapper">
+        <div class="form__container">
+          <div class="question__container">
+            <div class="question">I am not subject to any illness.</div>
+            <input type="text" class="answer" />
+          </div>
+
+          <div class="question__container">
+            <div class="question">I am not subject to any illness.</div>
+            <input type="text" class="answer" />
+          </div>
+
+          <div class="question__container">
+            <div class="question">I am not subject to any illness.</div>
+            <input type="text" class="answer" />
+          </div>
+        </div>
+      </main>
+
+      <footer class="footer"></footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
