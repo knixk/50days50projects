42d075662d2681412d5accce17f6ddb19fa1611a
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-11-27 10:42:11 +0530

    adding get and post query fns

--- waiver-node/index.js
+++ waiver-node/index.js
@@ -10,38 +10,71 @@ const con = mysql.createConnection({
 });
 
 const selectDB = "use waiver_form;";
-const getSubmissions = "select * from submissions;";
+
+const postASubmission = (con, data) => {
+  const query = `
+    INSERT INTO submissions (template_id, event_id, submission_data, name, email, mobile_number)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `;
+
+  con.query(
+    query,
+    [
+      data.template_id,
+      data.event_id,
+      data.submission_data,
+      data.name,
+      data.email,
+      data.mobile_number,
+    ],
+    (err, result) => {
+      if (err) throw err;
+      console.log("Inserted ID:", result.insertId);
+    }
+  );
+
+  console.log("insertion finished..");
+};
+
+const getSubmissions = (con, id = NULL) => {
+  const getSubmissionsQuery =
+    id != NULL
+      ? `select * from submissions where id = ${id};`
+      : "select * from submissions;";
+};
 
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
-
-  let res = con.query(selectDB, function (err, result) {
-    if (err) throw err;
-    // console.log("Result: " + result);
-  });
-
-  let submissions = con.query(getSubmissions);
-  if (!submissions) {
-    console.error("err");
-  }
-
-  console.log(submissions);
 });
 
 app.listen(port, () => {
   console.log(`app running on port: ${port}..`);
 });
 
-app.get("/", (req, res) => {
-  res.send("hello word").status(200);
+app.get("/submissions", (req, res) => {
+  getSubmissions(con);
+  // res.send("hello word").status(200);
 });
 
 // write an api to put data in db
 
 // write an api to query data from db
-app.post("/submit", (req, res) => {
+app.post("/submissions", (req, res) => {
+  const data = {
+    template_id: 1,
+    event_id: 2,
+    submission_data: JSON.stringify({ answer: "Example Answer" }),
+    name: "John Doe",
+    email: "john@example.com",
+    mobile_number: "1234567890",
+  };
+
+  postASubmission(con, data);
+
+  /*
   res.status(200).json({
     msg: "form was submitted",
   });
+  */
 });
---
+++ waiver-node/temp.js
@@ -0,0 +1,17 @@
+
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+
+//   let res = con.query(selectDB, function (err, result) {
+//     if (err) throw err;
+//     // console.log("Result: " + result);
+//   });
+
+//   let submissions = con.query(getSubmissions);
+//   if (!submissions) {
+//     console.error("err");
+//   }
+
+//   console.log(submissions);
+// });
