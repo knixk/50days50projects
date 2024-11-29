7f7ca1d90148a8c5b6eb3e3191dcd80346bcd487
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-11-29 14:49:26 +0530

    css changes

--- waiver-react/src/pages/Home.jsx
+++ waiver-react/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 // const logo = 'https://dypdvfcjkqkg2.cloudfront.net/large/5862799-1989.jpg'
+import { useNavigate, Link } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="home__container">
       <main className="main__container">
@@ -10,6 +13,10 @@ function Home() {
         <p className="sub__heading">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore{" "}
         </p>
+
+        <div className="btn__container">
+          <Link to={'/form?center=12'} className="btn fill-btn">Fill the form</Link>
+        </div>
       </main>
     </div>
   );
