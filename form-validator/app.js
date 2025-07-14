4355796e420b00cc6aece2f856fb006ef7253a91
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-07-14 14:47:24 +0530

    ch

--- form-validator/app.js
+++ form-validator/app.js
@@ -1,3 +1,44 @@
 // Let's hit some JS code!!!
 
-const form = document.getElementById('form')
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// show input error message
+function showError(input, message) {
+  // we can targe the element above like this..
+  const formControl = input.parentElement;
+  // we added this classname manually here..
+  formControl.className = "form-control error";
+
+  // when the error class is added to small tag is visible
+  //   Q. how it will know which small to target??
+  /*
+  
+  since we are under the scope of this formControl,
+  we do not need to specify which small tag it is..
+  */
+ 
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// show input success message
+function showSuccess() {}
+
+// Event listeners
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //   console.log("submit");
+  //   console.log(e);
+  //
+  //   console.log(username.value);
+
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+});
