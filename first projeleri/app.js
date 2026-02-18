b911d14c743ba266905debd94848e4c937b762ea
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2026-02-18 17:38:44 +0530

    good

--- first projeleri/app.js
+++ first projeleri/app.js
@@ -4,18 +4,28 @@
 we an onload function so that we know our window has been succesfully painted onto the browser, then we perform the actions we intend to.
 */
 
-
 window.onload = function () {
   // defining our canvas
   let canvas = document.querySelector("#hello-world-canvas"); // our canvas
-  let ctx = canvas.getContext("2d"); // our context ig, 
+  let ctx = canvas.getContext("2d"); // our context ig,
 
-    ctx.beginPath(); // start something
-    ctx.strokeStyle = 'darkred' // color of stroke
-    ctx.lineWidth = 5; // width
-    ctx.moveTo(70, 70); // start point
-    ctx.lineTo(200, 70); // end point
-    ctx.stroke(); // the actual draw fn
+  ctx.beginPath(); // start something
+  ctx.strokeStyle = "darkred"; // color of stroke
+  ctx.moveTo(30, 30); // start point
+  ctx.lineTo(80, 80); // end point
+  ctx.stroke(); // the actual draw fn
 
+  ctx.beginPath(); // start something
+  ctx.strokeStyle = "darkorange"; // color of stroke
+  ctx.moveTo(80, 80); // start point
+  ctx.lineTo(150, 80); // end point
+  ctx.stroke(); // the actual draw fn
+
+  ctx.beginPath(); // start something
+  ctx.strokeStyle = "blue"; // color of stroke
+  ctx.moveTo(150, 80); // start point
+  ctx.lineTo(170, 90); // end point
+  ctx.stroke(); // the actual draw fn
+
+  //   and so on
 };
-
--- first projeleri/clipboard.js
+++ first projeleri/clipboard.js
@@ -22,3 +22,31 @@ and the latter are the length and breadth
 window.onload = function () {
     
 }
+=======
+
+
+
+/* */
+
+/*
+we an onload function so that we know our window has been succesfully painted onto the browser, then we perform the actions we intend to.
+*/
+
+
+window.onload = function () {
+  // defining our canvas
+  let canvas = document.querySelector("#hello-world-canvas"); // our canvas
+  let ctx = canvas.getContext("2d"); // our context ig, 
+
+    ctx.beginPath(); // start something
+    ctx.strokeStyle = 'darkred' // color of stroke
+    ctx.lineWidth = 5; // width
+    ctx.moveTo(70, 70); // start point
+    ctx.lineTo(200, 70); // end point
+    ctx.stroke(); // the actual draw fn
+
+};
+
+
+
+
