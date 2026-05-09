a722b64238e0723a1861433273035693c5289dd7
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2026-05-09 17:37:45 +0530

    defining our classes

---
+++ game-in-js/app.js
@@ -0,0 +1,30 @@
+/* */
+/* Q. why aren't we using the Onload function? */
+
+window.addEventListener("load", function () {
+  // targeting our canvas
+  const canvas = document.getElementById("canvas1");
+  // console.log(canvas)
+  const SIZE = 500;
+  canvas.width = SIZE;
+  canvas.height = SIZE;
+
+  //   defining the classes we're going to use extensively and makes our lives easier
+
+  class InputHandler {}
+  class Projectile {}
+
+  class Particle {}
+
+  class Player {}
+
+  class Enemy {}
+
+  class Layer {}
+
+  class Background {}
+
+  class UI {}
+
+  class Game {}
+});
